7980da6c71356b1e8e023bdfc843ac5153832361
mahdy <>
2021-01-23 23:21:35 +0330

    Renewing from  dir

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,17 @@
+#
+#   __  __       _         _         __  __ _                   _
+#  |  \/  | __ _| |__   __| |_   _  |  \/  (_)_ __ ______ _  __| | ___
+#  | |\/| |/ _` | '_ \ / _` | | | | | |\/| | | '__|_  / _` |/ _` |/ _ \
+#  | |  | | (_| | | | | (_| | |_| | | |  | | | |   / / (_| | (_| |  __/
+#  |_|  |_|\__,_|_| |_|\__,_|\__, | |_|  |_|_|_|  /___\__,_|\__,_|\___|
+#                            |___/
+#
+# This file is a part of `github.com/mahdymirzade/dotfiles`.
+#
+# Main reference:
+# https://qutebrowser.org/doc/help/configuring.html
+#
+#
 # Custom Settings
 config.set("colors.webpage.darkmode.enabled", True)
 config.set("auto_save.session", True)
